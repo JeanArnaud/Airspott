72f106a6953a301869cd1410cb4111d646ca0dcd
Severin Burgstaller <severin@severinburgstaller.com>
2015-12-23 11:24:20 +0100

    email verification

--- common/models/customer.js
+++ common/models/customer.js
@@ -1,3 +1,24 @@
-module.exports = function(Customer) {
+var path = require('path');
+
+module.exports = function (Customer) {
+
+  Customer.afterRemote('create', function (context, user, next) {
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: 'noreply@airspott.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/templates/email/customer-activation.ejs'),
+      redirect: "#",
+      user: user
+    };
+
+    user.verify(options, function (err, response) {
+      if (err) return next(err);
+
+      next(null, user);
+    });
+
+  });
 
 };
