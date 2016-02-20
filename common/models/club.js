940ac695cb7cfc1bb49ed187039fecbbdb8be94c
Severin Burgstaller <severin@severinburgstaller.com>
2016-02-20 13:00:45 +0100

    added gulp task to auto-generate api services

--- common/models/club.js
+++ common/models/club.js
@@ -1,3 +1,39 @@
-module.exports = function(Club) {
+module.exports = function (Club) {
+
+    Club.search = function (query, amount, date, offers, cb) {
+        query = new RegExp('.*' + query + '.*', "i");
+        amount = amount || 1;
+        date = date || new Date();
+        offers = offers || [];
+
+        console.log(arguments);
+
+        var whereFilter = {
+            //or: {name: {like: query}}
+            name: {like: query}
+        };
+
+
+        Club.find({
+            where: whereFilter,
+            include: ['address', {relation: 'media', scope: {limit: 1, orderBy: 'order DESC'}}]
+        }, cb);
+    };
+
+    Club.remoteMethod(
+        'search',
+        {
+            accepts: [
+                {arg: 'query', type: 'string', required: true},
+                {arg: 'amount', type: 'number'},
+                {arg: 'date', type: 'date'},
+                {arg: 'offers', type: 'array'}
+            ],
+            returns: {arg: 'clubs', type: 'array', root: true},
+            http: {
+                verb: "GET"
+            }
+        }
+    );
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,36 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var exec = require('gulp-exec');
 
 var paths = {
-  sass: ['./client/scss/**/**/*.scss']
+    sass: ['./client/scss/**/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'apiservices:generate']);
 
 gulp.task('sass', function (done) {
-  gulp.src('./client/scss/airspott.club.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(rename({extname: '.min.css'}))
-    .pipe(gulp.dest('./client/club/css/'));
+    gulp.src('./client/scss/airspott.club.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./client/club/css/'));
 
-  gulp.src('./client/scss/airspott.shop.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(rename({extname: '.min.css'}))
-    .pipe(gulp.dest('./client/shop/css/'))
-    .on('end', done);
+    gulp.src('./client/scss/airspott.shop.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./client/shop/css/'))
+        .on('end', done);
+});
+
+gulp.task('apiservices:generate', function (cb) {
+    gulp.src('.')
+        .pipe(exec('lb-ng <%= file.path %>/server/server.js <%= file.path %>/client/common/js/api.js -m "com.airspott.api"'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.sass, ['sass']);
 });
