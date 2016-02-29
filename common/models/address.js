9df5f04a123c21baaaf7d776cc8aa7f0cb3d5de3
Severin Burgstaller <MarcSeverinManfred.Burgstaller@s-itsolutions.at>
2016-02-29 13:39:26 +0100

    adding geopoints automatically now

--- common/models/address.js
+++ common/models/address.js
@@ -1,3 +1,19 @@
-module.exports = function(Address) {
+var nominatim = require('nominatim');
 
+module.exports = function (Address) {
+    Address.observe('before save', function (ctx, next) {
+        var data = ctx.isNewInstance ? ctx.instance : ctx.data;
+
+        nominatim.search({
+            q: data.address + ", " + data.zip + " " + data.city + ", " + data.country
+        }, function (err, opts, results) {
+            if (err) return next(err);
+
+            if (results.length) {
+                data.latLon = {lat: results[0].lat, lng: results[0].lon};
+            }
+
+            next();
+        });
+    });
 };
